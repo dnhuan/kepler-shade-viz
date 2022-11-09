ac8180fe44f6cea6485448fe898f10e79f66a3aa
dnhuan <huandngoc@gmail.com>
2022-11-09 03:15:22 -0700

    Add support for multiple datasets

--- src/components/load-data-modal/city-button.js
+++ src/components/load-data-modal/city-button.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Button } from "kepler.gl/components";
 import { addDataToMap } from "kepler.gl/actions";
+import KeplerGlSchema from "kepler.gl/schemas";
 import { store } from "../../store";
 
-export default function CityButton({
-	cityName,
-	cityURL,
-	cityConfigURL,
-	onSelectCity,
-}) {
+export default function CityButton({ cityName, cityURL, cityConfigURL }) {
 	async function onSelectCityClick() {
 		console.log(`cityURL`, cityURL);
 		console.log(`cityConfigURL`, cityConfigURL);
 
 		try {
+			// get current map state
+			const { datasets, config } = KeplerGlSchema.save(
+				store.getState().keplerGl.shademap
+			);
+
+			// get new data
 			const [cityDataRaw, cityConfigRaw] = await Promise.all([
 				fetch(cityURL),
 				fetch(cityConfigURL),
@@ -24,31 +26,14 @@ export default function CityButton({
 			]);
 			console.log(cityData, cityConfig);
 
-			store.dispatch(
-				addDataToMap({
-					// datasets
-					datasets: {
-						info: {
-							label: cityData.datasets[0].data.label,
-							id: cityData.datasets[0].data.id,
-						},
-						data: {
-							fields: cityData.datasets[0].data.fields,
-							rows: cityData.datasets[0].data.allData,
-						},
-					},
-					// option
-					option: {
-						centerMap: true,
-						readOnly: false,
-					},
-					// config
-					config: cityConfig.config,
-				})
-			);
+			// merge new data with current map state
+			const newDatasets = [].concat(datasets, cityData.datasets);
+
+			// add new data to map
+			const mapToLoad = KeplerGlSchema.load(newDatasets, config);
+			store.dispatch(addDataToMap(mapToLoad));
 		} catch (err) {
 			console.log(err);
-			alert(err);
 		}
 	}
 
